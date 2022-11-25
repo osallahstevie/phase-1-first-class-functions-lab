80412116e4b49bfeb81343f2797161a7db87c6b5
osallahstevie <osallahstevie@gmail.com>
2022-11-25 22:03:50 +0300

    Assignment on javascript advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(fare){
+    return (function(n){
+        return n*fare
+    })
+}
+let fareDoubler = createFareMultiplier(2)
+let fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, line){
+    return line(arrayOfDrivers)
+}
